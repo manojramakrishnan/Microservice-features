16a05ab2d6e7d05b02da1330faa6bfed2694d2d3
manojramakrishnan <manoj.rgv@gmail.com>
2023-03-26 11:11:55 +0530

    Create ResponseTemplateVO.java
    added response value object

---
+++ user-microservice/src/main/java/com/je/user/vo/ResponseTemplateVO.java
@@ -0,0 +1,15 @@
+package com.je.user.vo;
+
+import com.je.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+
+    private User user;
+    private Department department;
+}
